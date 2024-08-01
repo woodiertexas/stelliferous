374c6302a8775b4fd3395ff8a594539c2883530e
woodiertexas <woodietexas@protonmail.com>
2024-07-31 23:00:09 -0700

    remove old code

--- src/main/java/com/woodiertexas/planetarium/mixin/WorldRendererMixin.java
+++ src/main/java/com/woodiertexas/planetarium/mixin/WorldRendererMixin.java
@@ -20,57 +20,6 @@ import static com.woodiertexas.planetarium.Planetarium.MOD_ID;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
-	/*
-	@Unique
-	Tessellator tessellator = Tessellator.getInstance();
-	
-	@Unique
-	private final BufferBuilder bufferBuilder = tessellator.method_60827(VertexFormat.DrawMode.TRIANGLE_FAN, VertexFormats.POSITION_COLOR);
-	
-	@Unique
-	private void fade(float tickDelta, float brightness) {
-		float rainGradient = 1.0f - world.getRainGradient(tickDelta);
-		float transparency = this.world.getStarBrightness(tickDelta) * rainGradient * brightness;
-		if (transparency > 0.0f) {
-			RenderSystem.setShaderColor(transparency, transparency, transparency, transparency);
-		}
-	}
-	
-	private void buildBuffer(Matrix4f matrix4f, float size) {
-		tessellator.method_60827(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-		bufferBuilder.xyz(matrix4f, -size, -100.0F, size).uv0(0.0F, 0.0F);
-		bufferBuilder.xyz(matrix4f, size, -100.0F, size).uv0(1.0F, 0.0F); // u: 1.0
-		bufferBuilder.xyz(matrix4f, size, -100.0F, -size).uv0(1.0F, 1.0F); // u: 1.0, v: 1.0
-		bufferBuilder.xyz(matrix4f, -size, -100.0F, -size).uv0(0.0F, 1.0F); // v: 1.0
-	}
-	
-	
-
-	@Unique
-	private void renderPlanet(Identifier planet, float planetSize, double translateX, double translateY, MatrixStack matrices, World world, float tickDelta, float planetPhase, float brightness) {
-		matrices.push();
-		
-		matrices.rotate(Axis.X_POSITIVE.rotationDegrees(world.getSkyAngle(tickDelta) + planetPhase));
-		matrices.translate(translateX, translateY, 0.0);
-		Matrix4f matrix4f = matrices.peek().getModel();
-		
-		if (world.getTimeOfDay() % 24000L >= 11800) {
-			RenderSystem.setShaderTexture(0, planet);
-			
-			buildBuffer(matrix4f, planetSize);
-			fade(tickDelta, brightness);
-			
-			BufferRenderer.drawWithShader(bufferBuilder.method_60800());
-		} else {
-			RenderSystem.setShaderTexture(0, 0);
-			tessellator.method_60827(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-			BufferRenderer.drawWithShader(bufferBuilder.method_60800());
-		}
-		
-		matrices.pop();
-	}
-*/
-	
 	@Shadow
 	private @Nullable ClientWorld world;
 	
