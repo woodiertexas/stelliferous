99b1958e13e27a3d0fe0ffab848ef8e45deee21e
woodiertexas <woodietexas@protonmail.com>
2024-08-01 14:01:00 -0700

    remove redundant record methods

--- src/main/java/com/woodiertexas/planetarium/Planet.java
+++ src/main/java/com/woodiertexas/planetarium/Planet.java
@@ -1,23 +1,3 @@
 package com.woodiertexas.planetarium;
 
-public record Planet(float procession, float tilt, float texture_rotation, float size) {
-	@Override
-	public float procession() {
-		return this.procession;
-	}
-	
-	@Override
-	public float tilt() {
-		return this.tilt;
-	}
-	
-	@Override
-	public float texture_rotation() {
-		return this.texture_rotation;
-	}
-	
-	@Override
-	public float size() {
-		return this.size;
-	}
-}
+public record Planet(float procession, float tilt, float texture_rotation, float size) {}
