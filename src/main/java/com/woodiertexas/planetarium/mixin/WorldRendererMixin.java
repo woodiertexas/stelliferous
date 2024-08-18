2decd76460c7dbfe647109e3042c3224aea274f1
woodiertexas <woodietexas@protonmail.com>
2024-08-17 21:28:46 -0700

    partial backport of planetarium improvements made by oro pt2

--- src/main/java/com/woodiertexas/planetarium/mixin/WorldRendererMixin.java
+++ src/main/java/com/woodiertexas/planetarium/mixin/WorldRendererMixin.java
@@ -1,22 +1,14 @@
 package com.woodiertexas.planetarium.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.BufferRenderer;
-import com.mojang.blaze3d.vertex.Tessellator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-import com.mojang.blaze3d.vertex.VertexFormats;
+import java.util.Map;
+
 import com.woodiertexas.planetarium.PlanetInfo;
+import com.woodiertexas.planetarium.PlanetManager;
 import com.woodiertexas.planetarium.Planetarium;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.world.ClientWorld;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Axis;
-import net.minecraft.world.World;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
+import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -24,7 +16,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static com.woodiertexas.planetarium.Planetarium.MOD_ID;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.BufferBuilderStorage;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
@@ -32,34 +33,20 @@ public class WorldRendererMixin {
 	private @Nullable ClientWorld world;
 	
 	@Unique
-	private static final Identifier MERCURY = new Identifier(MOD_ID, "textures/environment/mercury.png");
+	private PlanetManager planetarium$planetManager;
 	
-	@Unique
-	private static final Identifier VENUS = new Identifier(MOD_ID, "textures/environment/venus.png");
-	
-	@Unique
-	private static final Identifier MARS = new Identifier(MOD_ID, "textures/environment/mars.png");
-	
-	@Unique
-	private static final Identifier JUPITER = new Identifier(MOD_ID, "textures/environment/jupiter.png");
-	
-	@Unique
-	private static final Identifier SATURN = new Identifier(MOD_ID, "textures/environment/saturn.png");
-	
-	@Unique
-	private static final Identifier URANUS = new Identifier(MOD_ID, "textures/environment/uranus.png");
-	
-	@Unique
-	private static final Identifier NEPTUNE = new Identifier(MOD_ID, "textures/environment/neptune.png");
-	
-	@Unique
-	private static final Identifier NORTH_STAR = new Identifier("minecraft", "textures/item/nether_star.png");
-	
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void createPlanetManager(MinecraftClient client, EntityRenderDispatcher entityRenderDispatcher, BlockEntityRenderDispatcher blockEntityDispatcher, BufferBuilderStorage bufferBuilders, CallbackInfo ci) {
+		this.planetarium$planetManager = new PlanetManager();
+		ResourceLoader.get(ResourceType.CLIENT_RESOURCES).registerReloader(this.planetarium$planetManager);
+	}
 	
 	@Inject(method = "renderSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getStarBrightness(F)F"))
-	private void renderPlanets(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Camera preStep, boolean skipRendering, Runnable preRender, CallbackInfo ci) {
-		assert world != null;
+	private void renderCelestialObjects(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Camera preStep, boolean skipRendering, Runnable preRender, CallbackInfo ci) {
 		
-		Planetarium.renderPlanet(matrices, MERCURY, new PlanetInfo(0.0F, 0.0F, 0.0F, 0.0F, 10.0F, null), tickDelta, world);
+		assert world != null;
+		for (Map.Entry<Identifier, PlanetInfo> entry : planetarium$planetManager.getPlanets().entrySet()) {
+			Planetarium.renderPlanet(matrices, entry.getKey(), entry.getValue(), tickDelta, world);
+		}
 	}
 }
