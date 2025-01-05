65b3044820b9d86266d95ef9fe5230b55cc8f3c6
woodiertexas <woodietexas@protonmail.com>
2025-01-05 02:26:34 -0800

    removed datadriving stuff and made planets work better compared to the behavior of planetarium v1.2.1

--- src/main/java/com/woodiertexas/planetarium/Planetarium.java
+++ src/main/java/com/woodiertexas/planetarium/Planetarium.java
@@ -1,53 +1,41 @@
 package com.woodiertexas.planetarium;
 
-import org.joml.Matrix4f;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.BufferRenderer;
 import com.mojang.blaze3d.vertex.Tessellator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormats;
-
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Axis;
+import org.joml.Matrix4f;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Planetarium {
-	public static final Logger LOGGER = LoggerFactory.getLogger("Planetarium");
 	public static final String MOD_ID = "planetarium";
 	
-	public static void renderPlanet(MatrixStack matrices, Identifier id, PlanetInfo planetInfo, float tickDelta, ClientWorld world) {
+	public static void renderPlanet(MatrixStack matrices, Identifier planetId, float procession, float offset, float textureRotation, float size, float tickDelta, ClientWorld world) {
 		matrices.push();
 		
-		// First, line planet up where the sun is in the sky
-		matrices.multiply(Axis.Y_NEGATIVE.rotationDegrees(0.0F));
+		matrices.multiply(Axis.X_POSITIVE.rotationDegrees(world.getSkyAngle(tickDelta) + procession));
 		
-		// Second, change the orbital tilt of the planet
-		matrices.multiply(Axis.Y_POSITIVE.rotationDegrees(planetInfo.tilt())); // tilt
+		matrices.multiply(Axis.Z_POSITIVE.rotationDegrees(offset));
 		
-		// Third, set the angle of the planet in the sky and offset it.
-		matrices.multiply(Axis.X_NEGATIVE.rotationDegrees(world.getSkyAngle(tickDelta) * 360.0F + planetInfo.procession())); // procession
-		
-		matrices.multiply(Axis.Z_POSITIVE.rotationDegrees(planetInfo.inclination())); // inclination
-		
-		// Finally, change the rotation of the planet texture
-		matrices.multiply(Axis.Y_POSITIVE.rotationDegrees(planetInfo.texture_rotation()));
+		matrices.multiply(Axis.Y_POSITIVE.rotationDegrees(textureRotation));
 		
 		if (world.getTimeOfDay() % 24000L >= 11800) {
 			Matrix4f matrix4f = matrices.peek().getModel();
-			RenderSystem.setShaderTexture(0, planetInfo.getTexture(id));
+			RenderSystem.setShaderTexture(0, planetId);
 			
 			final BufferBuilder bufferBuilder = Tessellator.getInstance().getBufferBuilder();
 			bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-			float finalPlanetSize = planetInfo.size() * 0.75F;
-			bufferBuilder.vertex(matrix4f, -finalPlanetSize, -100.0F, finalPlanetSize).uv(0.0F, 0.0F).next();
-			bufferBuilder.vertex(matrix4f, finalPlanetSize, -100.0F, finalPlanetSize).uv(1.0F, 0.0F).next(); // u: 1.0
-			bufferBuilder.vertex(matrix4f, finalPlanetSize, -100.0F, -finalPlanetSize).uv(1.0F, 1.0F).next(); // u: 1.0, v: 1.0
-			bufferBuilder.vertex(matrix4f, -finalPlanetSize, -100.0F, -finalPlanetSize).uv(0.0F, 1.0F).next(); // v: 1.0
+			bufferBuilder.vertex(matrix4f, -size, -100.0F, size).uv(0.0F, 0.0F).next();
+			bufferBuilder.vertex(matrix4f, size, -100.0F, size).uv(1.0F, 0.0F).next(); // u: 1.0
+			bufferBuilder.vertex(matrix4f, size, -100.0F, -size).uv(1.0F, 1.0F).next(); // u: 1.0, v: 1.0
+			bufferBuilder.vertex(matrix4f, -size, -100.0F, -size).uv(0.0F, 1.0F).next(); // v: 1.0
 			
 			
 			float rainGradient = 1.0f - world.getRainGradient(tickDelta);
--- src/main/java/com/woodiertexas/planetarium/mixin/WorldRendererMixin.java
+++ src/main/java/com/woodiertexas/planetarium/mixin/WorldRendererMixin.java
@@ -1,62 +1,75 @@
 package com.woodiertexas.planetarium.mixin;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.BufferRenderer;
-import com.mojang.blaze3d.vertex.Tessellator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-import com.mojang.blaze3d.vertex.VertexFormats;
-import com.woodiertexas.planetarium.PlanetInfo;
-import com.woodiertexas.planetarium.PlanetManager;
 import com.woodiertexas.planetarium.Planetarium;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Axis;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
-import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 	@Shadow
 	private @Nullable ClientWorld world;
 	
-	//@Unique
-	//private static final Identifier MERCURY = new Identifier(Planetarium.MOD_ID, "textures/planets/mercury.png");
+	@Unique
+	private static final Identifier MERCURY = new Identifier(Planetarium.MOD_ID, "textures/planets/mercury.png");
 	
 	@Unique
-	private PlanetManager planetarium$planetManager;
+	private static final Identifier VENUS = new Identifier(Planetarium.MOD_ID, "textures/planets/venus.png");
 	
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void createPlanetManager(MinecraftClient client, EntityRenderDispatcher entityRenderDispatcher, BlockEntityRenderDispatcher blockEntityDispatcher, BufferBuilderStorage bufferBuilders, CallbackInfo ci) {
-		this.planetarium$planetManager = new PlanetManager();
-		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(this.planetarium$planetManager);
-	}
+	@Unique
+	private static final Identifier MARS = new Identifier(Planetarium.MOD_ID, "textures/planets/mars.png");
+	
+	@Unique
+	private static final Identifier JUPITER = new Identifier(Planetarium.MOD_ID, "textures/planets/jupiter.png");
+	
+	@Unique
+	private static final Identifier SATURN = new Identifier(Planetarium.MOD_ID, "textures/planets/saturn.png");
+	
+	@Unique
+	private static final Identifier URANUS = new Identifier(Planetarium.MOD_ID, "textures/planets/uranus.png");
+	
+	@Unique
+	private static final Identifier NEPTUNE = new Identifier(Planetarium.MOD_ID, "textures/planets/neptune.png");
+	
+	@Unique
+	private static final Identifier NORTH_STAR = new Identifier("minecraft", "textures/item/nether_star.png");
 	
 	@Inject(method = "renderSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getStarBrightness(F)F"))
 	private void renderPlanets(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, Camera preStep, boolean skipRendering, Runnable preRender, CallbackInfo ci) {
 		assert world != null;
 		
-		for (Map.Entry<Identifier, PlanetInfo> entry : planetarium$planetManager.getPlanets().entrySet()) {
-			Planetarium.renderPlanet(matrices, entry.getKey(), entry.getValue(), tickDelta, world);
-		}
+		Planetarium.renderPlanet(matrices, MERCURY, 140.00F, -30.00F, 0.03F, 5.00F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, VENUS, -177.00F, 20.00F, 2.64F, 6.50F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, MARS, 65.00F, 5.00F, 25.19F, 7.50F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, JUPITER, 15.45F, 50.00F, 3.13F, 11.00F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, SATURN, -28.57F, 30.00F, 26.73F, 10.40F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, URANUS, -67.90F, -43.20F, 82.23F, 6.40F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, NEPTUNE, -56.00F, -26.50F, 28.32F, 6.40F, tickDelta, world);
+		Planetarium.renderPlanet(matrices, NORTH_STAR, 0.00F, -86.50F, 0.00F, 1.35F, tickDelta, world);
 	}
+	
+	@Inject(method = "renderEndSky", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;depthMask(Z)V"))
+	private void renderPlanetsInEnd(MatrixStack matrices, CallbackInfo ci) {
+		assert world != null;
+		Planetarium.renderPlanet(matrices, MERCURY, 140.00F, -30.00F, 0.03F, 5.00F, world);
+	}
+	
+	/*
+	@ModifyVariable(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("STORE"), ordinal = 5)
+	float changeSunAndMoonSize(float moonSize) {
+		return 8.00F;
+	}
+	 */
 }
